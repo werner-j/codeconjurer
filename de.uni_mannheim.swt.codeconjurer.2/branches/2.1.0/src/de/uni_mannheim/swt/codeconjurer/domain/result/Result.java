15242550d9bcb7e75c334817dd3066b5560ec659
Werner Janjic <werner.janjic@informatik.uni-mannheim.de>
2011-08-04 11:40:10 +0000

    branches/2.1.0: Create development branch from trunk
    - Add an unchanged copy of trunk (45 files) as branches/2.1.0
    
    Original SVN log message: The next feature-enhanced version
    Imported from SourceForge SVN r19
    https://sourceforge.net/p/codeconjurer/code/19/

---
+++ de.uni_mannheim.swt.codeconjurer.2/branches/2.1.0/src/de/uni_mannheim/swt/codeconjurer/domain/result/Result.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright (c) 2007-2011
+ * University of Mannheim, Chair for Software-Engineering
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Werner Janjic -- initial development and documentation
+ */
+package de.uni_mannheim.swt.codeconjurer.domain.result;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Observable;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jdt.core.dom.BodyDeclaration;
+
+import com.merotronics.merobase.ws.action.ResultBean;
+
+/**
+ * @author Werner Janjic
+ * 
+ */
+public class Result extends Observable {
+
+	private Logger logger = Logger.getLogger(Result.class);
+
+	private HashMap<String, ResultItem> resultItems = new HashMap<String, ResultItem>();
+	private HashMap<String, String> successfulSources = new HashMap<String, String>();
+
+	private Calendar creation;
+
+	/**
+	 * Default Constructor
+	 */
+	public Result() {
+		super();
+		creation = Calendar.getInstance(Locale.getDefault());
+		creation.setTimeInMillis(System.currentTimeMillis());
+	}
+
+	/**
+	 * Returns the time and date the result was created
+	 * 
+	 * @return
+	 */
+	public String getCreationDate() {
+		SimpleDateFormat formatter = new SimpleDateFormat(
+				"E yyyy-MM-dd 'at' HH:mm zzz");
+		return formatter.format(creation.getTime());
+	}
+
+	/**
+	 * This method may be used to add a list of results items to this result
+	 * 
+	 * @param results
+	 */
+	public void addResultList(ArrayList<ResultBean> results) {
+		for (ResultBean result : results) {
+			String shortUrl = result.getShortUrl();
+			ResultItem resultItem = new ResultItem(shortUrl, result.getName());
+			resultItems.put(shortUrl, resultItem);
+		}
+		setChanged();
+		notifyObservers();
+	}
+
+	/**
+	 * This method may be used to <b>override</b> the list of result items
+	 * 
+	 * @param results
+	 */
+	public void setResultList(ArrayList<ResultBean> results) {
+		resultItems = new HashMap<String, ResultItem>();
+		for (ResultBean result : results) {
+			String shortUrl = result.getShortUrl();
+			ResultItem resultItem = new ResultItem(shortUrl, result.getName());
+			resultItems.put(shortUrl, resultItem);
+		}
+		setChanged();
+		notifyObservers();
+	}
+
+	/**
+	 * Returns the result items
+	 * 
+	 * @return
+	 */
+	public ResultItem[] getResultItems() {
+		ResultItem[] results = new ResultItem[resultItems.values().size()];
+		return resultItems.values().toArray(results);
+	}
+
+	/**
+	 * Returns the sourcecode for the provided URL
+	 * 
+	 * @param shortUrl
+	 * @return
+	 */
+	public String getSource(String shortUrl) {
+		if (successfulSources.containsKey(shortUrl)) {
+			return "// Sourcecode for " + shortUrl
+					+ "\r\n// not available from cache.";
+		} else {
+			return successfulSources.get(shortUrl);
+		}
+	}
+
+	/**
+	 * Set the source code for the given URL
+	 * 
+	 * @param shortUrl
+	 * @param source
+	 */
+	public void addSource(String shortUrl, String source) {
+		if (source != null && !source.equals("null")
+				&& !source.contains("/** Source could not be fetched */")) {
+			logger.debug("Add source for " + shortUrl);
+			resultItems.get(shortUrl).setSource(
+					"// Brought to you by merobase.com\r\n// Origin: \r\n// "
+							+ shortUrl + "\r\n" + source);
+			successfulSources.put(shortUrl, source);
+		} else {
+			logger.debug("Source for " + shortUrl + " not available");
+			source = "// Sourcecode for " + shortUrl + "\r\n// not available.";
+			resultItems.get(shortUrl).setSource(source);
+			// successfulSources.put(shortUrl, source);
+			successfulSources.remove(shortUrl);
+		}
+		setChanged();
+		notifyObservers();
+	}
+
+	/**
+	 * Returns the number of successfully fetched sources
+	 * 
+	 * @return
+	 */
+	public int getNumberOfSuccessfullyFetchedSources() {
+		return successfulSources.size();
+	}
+
+	/**
+	 * Returns the corresponding CompilationUnit object to the given shortUrl
+	 * 
+	 * @param shortUrl
+	 * @return
+	 */
+	public BodyDeclaration getTypeRoot(String shortUrl) {
+		return resultItems.get(shortUrl).getTypeRoot();
+	}
+
+	/**
+	 * Returns the number of stored results
+	 * 
+	 * @return
+	 */
+	public int size() {
+		return resultItems.size();
+	}
+
+}
---
+++ de.uni_mannheim.swt.codeconjurer.2/branches/2.1.0/src/de/uni_mannheim/swt/codeconjurer/domain/search/Search.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2007-2011
+ * University of Mannheim, Chair for Software-Engineering
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Werner Janjic -- initial development and documentation
+ */
+package de.uni_mannheim.swt.codeconjurer.domain.search;
+
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.ui.IEditorPart;
+
+import de.uni_mannheim.swt.codeconjurer.domain.listener.SearchEvent;
+import de.uni_mannheim.swt.codeconjurer.domain.listener.SearchEventListener;
+import de.uni_mannheim.swt.codeconjurer.domain.result.Result;
+
+/**
+ * Interface for classes that perform searches on MB
+ * 
+ * @author Werner Janjic
+ */
+public abstract class Search extends Job {
+
+	protected Logger logger = Logger.getLogger(Search.class);
+
+	protected ArrayList<SearchEventListener> listeners = new ArrayList<SearchEventListener>();
+
+	protected Query query;
+	protected Result result;
+
+	private IEditorPart editor;
+
+	/**
+	 * Creates a Search
+	 * 
+	 * @param name
+	 */
+	public Search(IEditorPart editor, String name, Query query) {
+		super(name);
+		this.query = query;
+		this.editor = editor;
+		this.result = new Result();
+	}
+
+	/**
+	 * Returns the results of this search
+	 * 
+	 * @return
+	 */
+	public Result getSearchResult() {
+		return result;
+	}
+
+	public IEditorPart getEditor() {
+		return editor;
+	}
+
+	public void addSearchEventListener(SearchEventListener listener) {
+		this.listeners.add(listener);
+	}
+
+	protected void notifySearchEventListeners(SearchEvent event) {
+		logger.debug("Notify SearchEventListeners: " + event.toString());
+		for (SearchEventListener listener : listeners) {
+			listener.onEvent(event);
+		}
+	}
+
+}
