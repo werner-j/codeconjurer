a16acefff82c65b62a8d1dd43ff6eed3385a1199
Werner Janjic <werner.janjic@informatik.uni-mannheim.de>
2011-08-16 14:18:29 +0000

    branches/2.0.1: Add "Format Code on Insertion" preference
    - Re-add PreferenceConstants/PreferenceInitializer with new P_FORMAT
      option (default true) and CodeConjurerPreferences checkbox
    - Re-add SourceDragListener, which formats inserted code only when
      the preference is enabled
    
    Original SVN log message: Release 2.0.1
    Imported from SourceForge SVN r39
    https://sourceforge.net/p/codeconjurer/code/39/

---
+++ de.uni_mannheim.swt.codeconjurer.2/branches/2.0.1/src/de/uni_mannheim/swt/codeconjurer/domain/preferences/PreferenceConstants.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2007-2011
+ * University of Mannheim, Chair for Software-Engineering
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Werner Janjic -- initial development and documentation
+ */
+package de.uni_mannheim.swt.codeconjurer.domain.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+
+	public static final String P_SERVER = "merobaseServer";
+
+	public static final String P_USERNAME = "username";
+
+	public static final String P_PASSWORD = "password";
+
+	public static final String P_RESULTS = "numberOfResultsToRetrieve";
+
+	public static final String P_FORMAT = "formatSourceOnInsert";
+
+}
