da115d2cff81652d38d06a799693494ba4a77c03
Werner Janjic <werner.janjic@informatik.uni-mannheim.de>
2011-10-31 16:21:17 +0000

    tags/drag_n_drop: Add ResultTreeDragListener and drop action delegate
    - ResultTreeDragListener provides text, resource and plugin transfer
      data for the selected result
    - PluginDropActionDelegate stub logs received drops
    
    Original SVN log message: Merlin
    Imported from SourceForge SVN r67
    https://sourceforge.net/p/codeconjurer/code/67/

---
+++ de.uni_mannheim.swt.codeconjurer.2/tags/drag_n_drop/src/de/uni_mannheim/swt/codeconjurer/ui/dnd/PluginDropActionDelegate.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2007-2011
+ * University of Mannheim, Chair for Software-Engineering
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Werner Janjic -- initial development and documentation
+ */
+package de.uni_mannheim.swt.codeconjurer.ui.dnd;
+
+import org.apache.log4j.Logger;
+import org.eclipse.ui.part.IDropActionDelegate;
+
+/**
+ * @author Werner Janjic
+ * 
+ */
+public class PluginDropActionDelegate implements IDropActionDelegate {
+
+	private Logger logger = Logger.getLogger(PluginDropActionDelegate.class);
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.part.IDropActionDelegate#run(java.lang.Object,
+	 * java.lang.Object)
+	 */
+	@Override
+	public boolean run(Object source, Object target) {
+		logger.debug("Received " + source.toString() + " for "
+				+ target.toString());
+		return false;
+	}
+
+}
---
+++ de.uni_mannheim.swt.codeconjurer.2/tags/drag_n_drop/src/de/uni_mannheim/swt/codeconjurer/ui/dnd/ResultTreeDragListener.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2007-2011
+ * University of Mannheim, Chair for Software-Engineering
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Werner Janjic -- initial development and documentation
+ */
+package de.uni_mannheim.swt.codeconjurer.ui.dnd;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jdt.core.dom.BodyDeclaration;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.dnd.DragSourceEvent;
+import org.eclipse.swt.dnd.DragSourceListener;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.ui.part.PluginTransfer;
+import org.eclipse.ui.part.PluginTransferData;
+import org.eclipse.ui.part.ResourceTransfer;
+
+/**
+ * @author Werner Janjic
+ * 
+ */
+public class ResultTreeDragListener implements DragSourceListener {
+
+	private Logger logger = Logger.getLogger(ResultTreeDragListener.class);
+
+	private TreeViewer viewer;
+	private TreeItem[] selection;
+
+	/**
+	 * Standard constructor for this Listener
+	 * 
+	 * @param viewer
+	 */
+	public ResultTreeDragListener(TreeViewer viewer) {
+		super();
+		this.viewer = viewer;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.swt.dnd.DragSourceListener#dragStart(org.eclipse.swt.dnd.
+	 * DragSourceEvent)
+	 */
+	@Override
+	public void dragStart(DragSourceEvent event) {
+		selection = viewer.getTree().getSelection();
+		if (selection == null || selection.length == 0) {
+			event.doit = false;
+		}
+	}
+
+	/**
+	 * Set the transfer data. A method should be dropped to the text editor,
+	 * thus we need a TextTransfer. A type should go into the package explorer,
+	 * where we need a PluginTransfer.
+	 */
+	@Override
+	public void dragSetData(DragSourceEvent event) {
+		logger.debug(event.dataType + " requested by drop target.");
+
+		BodyDeclaration selectedElement = (BodyDeclaration) selection[0]
+				.getData();
+		if (TextTransfer.getInstance().isSupportedType(event.dataType)) {
+			event.data = selectedElement.toString();
+			logger.debug("TextTransfer issued");
+		} else if (ResourceTransfer.getInstance().isSupportedType(
+				event.dataType)) {
+			event.data = selectedElement;
+			logger.debug("ResourceTransfer issued");
+		} else if (PluginTransfer.getInstance().isSupportedType(event.dataType)) {
+			event.data = new PluginTransferData(
+					"de.uni_mannheim.swt.codeconjurer.ui.dnd.pluginDropAction",
+					selectedElement.toString().getBytes());
+			logger.debug("PluginTransfer issued");
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.swt.dnd.DragSourceListener#dragFinished(org.eclipse.swt.dnd
+	 * .DragSourceEvent)
+	 */
+	@Override
+	public void dragFinished(DragSourceEvent event) {
+		if (event.doit) {
+			logger.debug("Drag of "
+					+ viewer.getTree().getSelection()[0].getText()
+					+ " finished. Format the Sourcecode properly...");
+		}
+	}
+
+}
