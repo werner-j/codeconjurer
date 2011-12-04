25bd2d6953882151b987ed95d4ca5bc379507fdf
Werner Janjic <werner.janjic@informatik.uni-mannheim.de>
2011-12-04 20:07:03 +0000

    tags/drag_n_drop: Add result view actions and selection listener
    - Add PerformSearchFromViewAction, RefreshAction and ShowAdapterAction
      referenced by the new ResultView toolbar contributions
    - Add TreeSelectionChangedListener showing either the result source or
      its adapter code in the code preview
    - Add AskForClassNameThread, an input dialog asking for the name of
      the enclosing Java class
    
    Original SVN log message: Merlin
    Imported from SourceForge SVN r77
    https://sourceforge.net/p/codeconjurer/code/77/

---
+++ de.uni_mannheim.swt.codeconjurer.2/tags/drag_n_drop/src/de/uni_mannheim/swt/codeconjurer/ui/view/elements/actions/PerformSearchFromViewAction.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2007-2011
+ * University of Mannheim, Chair for Software-Engineering
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Werner Janjic -- initial development and documentation
+ */
+package de.uni_mannheim.swt.codeconjurer.ui.view.elements.actions;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+
+import de.uni_mannheim.swt.codeconjurer.application.CodeConjurer;
+
+/**
+ * @author Werner Janjic
+ * 
+ */
+public class PerformSearchFromViewAction implements IViewActionDelegate {
+
+	Logger logger = Logger.getLogger(this.getClass());
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+	 */
+	@Override
+	public void run(IAction action) {
+		logger.debug("Perform a search");
+		CodeConjurer.getInstance().search(false);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action
+	 * .IAction, org.eclipse.jface.viewers.ISelection)
+	 */
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IViewActionDelegate#init(org.eclipse.ui.IViewPart)
+	 */
+	@Override
+	public void init(IViewPart view) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
